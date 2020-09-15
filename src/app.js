b068f916e9cfdd114b5bf46aa9b9e24d0f022c9a
lmhgithi <lmhfox@foxmail.com>
2020-09-15 15:33:32 +0800

    [lmh] grep user id from url

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import "./style/index.scss";
 import {renderUserInfo, renderUserEducations} from "./render"
-const id=1;
+
+const query = window.location.pathname;
+const path = query.split("/");
+let id = 0;
+if (path[1] == "users") {
+    id=path[2];
+}
 
 renderUserInfo(id);
 renderUserEducations(id)
