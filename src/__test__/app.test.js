1bfa260c6c0cffd10fbade7ab01617e006e7906b
lmhgithi <lmhfox@foxmail.com>
2020-09-15 16:09:38 +0800

    [lmh] add tests

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,3 +1,22 @@
-test("add 1+1 is 2", () => {
-  expect(2).toBe(2);
+import { renderUserInfo, renderUserEducations } from "../render";
+import { getUserEducations, getUserInfo } from "../fetch";
+
+jest.mock("../fetch");
+
+describe("fetch data", () => {
+  test("should fetch data when render user info", async () => {
+    getUserInfo.mockImplementation(() => {});
+
+    renderUserInfo(1);
+
+    await expect(getUserInfo).toHaveBeenCalled();
+  });
+
+  test("should fetch data when render educations", async () => {
+    getUserEducations.mockImplementation(() => {});
+
+    renderUserEducations(1);
+
+    await expect(getUserEducations).toHaveBeenCalled();
+  });
 });
